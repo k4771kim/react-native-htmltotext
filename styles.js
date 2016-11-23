9bb68e3d6f3bf6b94b04b7a85bb19e2521481368
NikolaBorislavovHristov <nikola.b.hristov@gmail.com>
2016-11-23 17:43:12 +0200

    default container styles added

--- styles.js
+++ styles.js
@@ -1,6 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
     a: {
     },
     b: {
