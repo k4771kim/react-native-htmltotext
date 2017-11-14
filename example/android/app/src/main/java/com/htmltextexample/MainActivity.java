1ff5ded293bc3ddcc03dbce91f9e922300f357d2
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2017-11-14 13:53:10 +0200

    example added

---
+++ example/android/app/src/main/java/com/htmltextexample/MainActivity.java
@@ -0,0 +1,11 @@
+package com.htmltextexample;
+
+import com.facebook.react.ReactActivity;
+
+public final class MainActivity extends ReactActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        return "HtmlTextExample";
+    }
+}
